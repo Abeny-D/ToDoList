ca58ae8deb68c9b5eeaf0904d239067e09c17fdb
Your Name <abeniiidereje@gmail.com>
2025-07-24 00:40:22 +0300

    first commit

---
+++ src/component/add-task.tsx
@@ -0,0 +1,12 @@
+import { FaPlus } from "react-icons/fa";
+
+const AddTask = () => {
+    return (
+        <div>
+            <button className="btn btn-primary w-full rounded-2xl">Add new task <FaPlus /></button>
+
+        </div>
+    );
+};
+
+export default AddTask;
